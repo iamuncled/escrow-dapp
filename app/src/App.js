087db3be227cd0fa3c152c5394754044ea5c623f
Diego Fumero <diegofumero@Diegos-MacBook-Air.local>
2023-03-31 00:52:47 +0200

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -29,15 +29,16 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
-    const escrowContract = await deploy(signer, arbiter, beneficiary, value);
+    const depositInEth = parseFloat(document.getElementById('eth').value);
+    const depositInWei = ethers.utils.parseEther(depositInEth.toString());
+    const escrowContract = await deploy(signer, arbiter, beneficiary, depositInWei);
 
 
     const escrow = {
       address: escrowContract.address,
       arbiter,
       beneficiary,
-      value: value.toString(),
+      value: depositInWei.toString(),
       handleApprove: async () => {
         escrowContract.on('Approved', () => {
           document.getElementById(escrowContract.address).className =
@@ -68,8 +69,8 @@ function App() {
         </label>
 
         <label>
-          Deposit Amount (in Wei)
-          <input type="text" id="wei" />
+          Deposit Amount (in Ether)
+          <input type="text" id="eth" />
         </label>
 
         <div
